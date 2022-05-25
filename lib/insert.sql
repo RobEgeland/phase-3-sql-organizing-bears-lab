cb82697cd3f308619cf2e9651cad799e1104e2bb
Robert Egeland <robegeland@yahoo.com>
2022-05-24 21:45:44 -0500

    commit tree e93220d75daf61194d3e4f46fdf3f6368032ca2d

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ('Mr.Chocolate', 12, 'M', 'Brown', 'Angry', True);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ('Rowdy', 19, 'M', 'Black', 'Clalm', False);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ('Tabitha', 5, 'F', 'Brown', 'Friendly', True);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 22, 'M', 'Brown', 'Angry', False);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ('Melissa', 9, 'F', 'Grizzly', 'Angry', True);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ('Grinch', 18, 'M', 'Polar', 'Calm', True);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ('Wendy', 2, 'F', 'Brown', 'Friendly', True);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES (NULL, NULL, 'M', NULL, 'Angry', True);
